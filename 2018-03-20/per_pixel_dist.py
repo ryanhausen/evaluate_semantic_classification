1c06f4d56503e5da8daa4f029e6f6e473a675cde
Ryan Hausen <ryan.hausen@gmail.com>
2018-03-23 02:02:51 -0700

    Added Mean*(1-Std) graph

--- 2018-03-20/per_pixel_dist.py
+++ 2018-03-20/per_pixel_dist.py
@@ -162,7 +162,45 @@ for op_name, op in data_reductions:
 
     outputs.append(f)
 
+f = figure(x_range=[x_start, x_end],
+           y_range=[y_start, y_end],
+           width=GRAPH_WIDTH,
+           height=GRAPH_HEIGHT,
+           toolbar_location='above',
+           title=f'Mean * (1-Std)  Probabilities Per Class ')
 
+items = []
+for morphology in columns[2:]:
+    reduction_op = reductions.summary(m=reductions.mean(morphology), s=reductions.std(morphology))
+
+    canvas = datashader.Canvas(plot_width=x_end-x_start,
+                            plot_height=y_end-y_start,
+                            x_range=[x_start, x_end],
+                            y_range=[y_start, y_end])
+
+    agg = canvas.points(data, 'x', 'y', agg=reduction_op)
+    img = t_func.shade(agg.m * np.subtract(1.0, agg.s),
+                        cmap=Inferno256,
+                        span=[0,1],
+                        how='linear')
+
+    img_plt = f.image_rgba(image=[img.data],
+                        x=x_start,
+                        y=y_start,
+                        dw=width,
+                        dh=height,
+                        visible=morphology=='Disk')
+
+    items.append((morphology, [img_plt]))
+
+legend = Legend(items=items,
+                location=(0,250),
+                click_policy='hide',
+                )
+
+f.add_layout(legend, place='right')
+
+outputs.append(f)
 
 # ==============================================================================
 
@@ -170,201 +208,27 @@ for op_name, op in data_reductions:
 # ==============================================================================
 # Plot PDFs for all pixels
 f = figure(x_range=[-0.1, 1.1],
-        y_range=[-0.1, 1.1],
+        y_axis_type='log',
         width=GRAPH_WIDTH,
         height=GRAPH_HEIGHT,
         toolbar_location='above',
         title='PDFs For all Pixels')
 
 for morphology, color in zip(columns[2:], Spectral6):
-    results, edges = np.histogram(data[morphology].values, bins=100)
+    results, edges = np.histogram(data[morphology].values, bins=1000)
     x = edges[:-1] + np.diff(edges)/2
     y = results / results.sum()
     width = edges[1]-edges[0]
-    f.line(x,y, line_color=color, line_alpha=0.5)
+    f.line(x,y, line_color=color, 
+                line_alpha=0.5, 
+                line_width=4, 
+                legend=morphology)
 
+f.legend.location = 'bottom_right'
+f.legend.click_policy = 'hide'
 outputs.append(f)
-
-
-
 # ==============================================================================
 
 output = column(outputs)
 show(output)
-sys.exit(0)
-
-
-
-
-
-
-
-
-# pixel in interest
-y, x = 60, 102
-
-#row = -1
-#for i in range(slices.shape[0]):
-    #climb rows
-#    if i%361==0:
-#        row += 1
-#        if row==(y-40):
-#            #made it
-#            print(i)
-#            sys.exit(0)
-#            plt.title(row)
-#            plt.imshow(in_slices[i,:,:], cmap='gray', origin='lower')
-#            plt.show()
-
-# row 60 starts at 7220
-# col 102 starts at 7220+102
-
-START_SLICE = 7220+(x-40)
-
-
-colls = {
-        '40':[[], [], [], [], [], []],
-        '20':[[], [], [], [], [], []],
-        '10':[[], [], [], [], [], []],
-        '4':[[], [], [], [], [], []],
-        '2':[[], [], [], [], [], []]
-        }
-names = ['Spheroid', 'Disk', 'Irregular', 'Point Source', 'Uknown', 'Background']
-
-spatial_dist = np.zeros([40,40])
-
-for i in range(40):
-    for j in range(40):
-        idx_y, idx_x = -(i+1), -(j+1)
-        rec_x, rec_y = (40+idx_x), (40+idx_y)
-        singl_slice = slices[START_SLICE,:,:,:]
-        prob_vector = slices[START_SLICE,idx_y,idx_x,:]
-
-        args = np.argsort(-prob_vector)
-        vals = args[:1]
-        spatial_dist[idx_y, idx_x] = prob_vector[1]
-
-        arg_count = 0
-        # inner 40x40
-        for p, coll in zip(prob_vector, colls['40']):
-            if arg_count in vals:
-                coll.append(p)
-            arg_count += 1
-
-        arg_count = 0
-        # inner 20x20
-        if (i < 30 and i > 9) and (j < 30 and j > 9):
-            for p, coll in zip(prob_vector, colls['20']):
-                if arg_count in vals:
-                    coll.append(p)
-                arg_count += 1
-
-        arg_count = 0
-        # inner 10x10
-        if (i < 25 and i > 14) and (j < 25 and  j > 14):
-            for p, coll in zip(prob_vector, colls['10']):
-                if arg_count in vals:
-                    coll.append(p)
-                arg_count += 1
-
-        arg_count = 0
-        # inner 4x4
-        if (i < 22 and i > 17) and (j < 22 and j > 17):
-            for p, coll in zip(prob_vector, colls['4']):
-                if arg_count in vals:
-                    coll.append(p)
-                arg_count += 1
-
-        arg_count = 0
-        # inner 2x2
-        if (i < 21 and i > 18) and (j < 21 and j > 18):
-            for p, coll in zip(prob_vector, colls['2']):
-                if arg_count in vals:
-                    coll.append(p)
-                arg_count += 1
-
-
-        START_SLICE += 1
-    START_SLICE += (361-40)
-
-all_plots = [Div(text='<h1 style="text-align:center">Top 2 Classifications</h1>', width=1200)]
-bins = [100, 25, 10, 4, 4]
-px_counts = ['2', '4', '10', '20', '40']
-px_counts = ['40', '20', '10', '4', '2']
-
-output_file("pixel_dist.html")
-for k, b in zip(px_counts, bins):
-
-    title = '<h3 style="text-align:center">{}x{} Classification Distributions for: ({},{})</h3>'.format(k,k,x,y)
-    f_pdf = figure(title='PDF',
-                   x_axis_label='P(pixel=class)',
-                   y_axis_label='Normalized Pixel Count',
-                   x_range=[0,1.1],
-                   y_range=[0,1.1],
-                   width=600)
-
-    f_cdf = figure(title='CDF',
-                   x_axis_label='P(pixel=class)',
-                   y_axis_label='Normalized Pixel Count',
-                   x_range=[0,1.1],
-                   y_range=[0,1.1],
-                   width=600)
-
-    for c, n, color in zip(colls[k], names, Spectral6):
-        if len(c) > 0:
-            _y, _x = np.histogram(c, bins=len(c) if len(c) < 16 else b)
-            _x = _x[:-1] + np.diff(_x)/2
-            _y = _y/_y.sum()
-
-            #f_pdf.line(_x, _y, legend=n)
-            #f_cdf.line(_x, _y.cumsum(), legend=n)
-            if len(c)>1:
-                f_pdf.line(_x, _y, legend=n, line_color=color, line_width=4)
-                f_cdf.line(_x, _y.cumsum(), legend=n, line_color=color, line_width=4)
-            else:
-                f_pdf.circle(_x, _y, legend=n, line_color=color, line_width=4)
-                f_cdf.circle(_x, _y.cumsum(), legend=n, line_color=color, line_width=4)
-
-    f_cdf.legend.location = 'top_right'
-    f_cdf.legend.click_policy = 'mute'
-    f_pdf.legend.location = 'top_right'
-    f_pdf.legend.click_policy = 'mute'
-
-
-    cmb = row(f_pdf, f_cdf)
-    f = column(Div(text=title, width=1200), cmb)
-    all_plots.append(f)
-
-f_all = column(*all_plots)
-show(f_all)
-
-print(spatial_dist.shape)
-output_file('spatial_dist.html', title='Spatial Distribution')
-f_spatial = figure(title='Disk Over Pixels',
-                   x_range=[0,40],
-                   y_range=[0,40])
-f_spatial.image(image=[spatial_dist], x=0, y=0, dw=40, dh=40, palette="Spectral11")
-show(f_spatial)
-
-
-#    f, (ax_pdf, ax_cdf) = plt.subplots(nrows=1,ncols=2)
-#    for axes in [ax_pdf, ax_cdf]:
-#        axes.set_xlim(0,1)
-#        axes.set_xlabel('P(pixel=class')
-#        axes.set_ylabel('Normalized Pixel Count')
-#    ax_pdf.set_title('PDF')
-#    ax_cdf.set_title('CDF')
-#
-#    plt.suptitle('{}x{} Classification Distributions for: ({},{})'.format(k,k,x,y))
-#
-#    for c, n in zip(colls[k], names):
-#        _y, _x = np.histogram(c, bins=b)
-#        _x = _x[:-1] + np.diff(_x)/2
-#        _y = _y/_y.sum()
-#        ax_pdf.plot(_x, _y, label=n)
-#        ax_cdf.plot(_x, _y.cumsum(), label=n)
-#    plt.legend()
-
-#plt.show()
-
 sys.exit(0)
