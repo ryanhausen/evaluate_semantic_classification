69dc14db1882112c06f6769649b2eb8cd6e421fb
Ryan Hausen <ryan.hausen@gmail.com>
2018-03-23 16:07:42 -0700

    WIP Segmap/Nonsegmap Pixel Dist

--- 2018-03-20/per_pixel_dist.py
+++ 2018-03-20/per_pixel_dist.py
@@ -5,12 +5,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.cm as cm
+import matplotlib.colors as clr
 from astropy.io import fits
 
 from bokeh.plotting import figure, output_file, show
-from bokeh.palettes import Spectral6, Inferno256, Category10
+from bokeh.palettes import Spectral6, Reds3, Inferno256, Category10
 from bokeh.models import Div, Legend
-from bokeh.layouts import column, row
+from bokeh.layouts import column, row, gridplot
 import datashader as ds
 import datashader.glyphs
 import datashader.transfer_functions as t_func
@@ -61,6 +63,7 @@ outputs.append(f)
 # ==============================================================================
 # Extract probabilites
 # inner 10x10 is from [15,25)
+
 columns = ['y', 'x', 'Spheroid', 'Disk', 'Irregular', 'Point Source', 'Unknown', 'Background']
 if 'points.csv' not in os.listdir('.'):
     roi_ys = np.arange(y_start, y_end, dtype=np.int32)
@@ -116,13 +119,15 @@ else:
 # Display a segmap based on probablilites
 # https://anaconda.org/jbednar/pipeline/notebook
 
+probs_grid = [[], []]
+
 data_reductions = [
     ('Mean', reductions.mean),
     ('Max', reductions.max),
     ('Standard Deviation', reductions.std)
 ]
 
-for op_name, op in data_reductions:
+for i, (op_name, op) in enumerate(data_reductions):
     f = figure(x_range=[x_start, x_end],
             y_range=[y_start, y_end],
             width=GRAPH_WIDTH,
@@ -160,29 +165,72 @@ for op_name, op in data_reductions:
 
     f.add_layout(legend, place='right')
 
-    outputs.append(f)
+    probs_grid[i//2].append(f)
 
-f = figure(x_range=[x_start, x_end],
+# mean * (1-std)
+# f = figure(x_range=[x_start, x_end],
+#            y_range=[y_start, y_end],
+#            width=GRAPH_WIDTH,
+#            height=GRAPH_HEIGHT,
+#            toolbar_location='above',
+#            title=f'Mean * (1-Std)  Probabilities Per Class ')
+
+# items = []
+# for morphology in columns[2:]:
+#     reduction_op = reductions.summary(m=reductions.mean(morphology), s=reductions.std(morphology))
+
+#     canvas = datashader.Canvas(plot_width=x_end-x_start,
+#                             plot_height=y_end-y_start,
+#                             x_range=[x_start, x_end],
+#                             y_range=[y_start, y_end])
+
+#     agg = canvas.points(data, 'x', 'y', agg=reduction_op)
+#     img = t_func.shade(agg.m * np.subtract(1.0, agg.s),
+#                         cmap=Inferno256,
+#                         span=[0,1],
+#                         how='linear')
+
+#     img_plt = f.image_rgba(image=[img.data],
+#                         x=x_start,
+#                         y=y_start,
+#                         dw=width,
+#                         dh=height,
+#                         visible=morphology=='Disk')
+
+#     items.append((morphology, [img_plt]))
+
+# legend = Legend(items=items,
+#                 location=(0,250),
+#                 click_policy='hide',
+#                 )
+
+# f.add_layout(legend, place='right')
+
+# outputs.append(f)
+
+
+f = figure(title='Segmap based mean P(c!=Background)>=P(c==Background)',
+           x_range=[x_start, x_end],
            y_range=[y_start, y_end],
            width=GRAPH_WIDTH,
            height=GRAPH_HEIGHT,
-           toolbar_location='above',
-           title=f'Mean * (1-Std)  Probabilities Per Class ')
+           toolbar_location='above')
 
 items = []
-for morphology in columns[2:]:
-    reduction_op = reductions.summary(m=reductions.mean(morphology), s=reductions.std(morphology))
+for morphology in columns[2:-1]:
+    reduction_op = reductions.summary(not_bkg=reductions.mean(morphology),
+                                      bkg=reductions.mean('Background'))
 
     canvas = datashader.Canvas(plot_width=x_end-x_start,
-                            plot_height=y_end-y_start,
-                            x_range=[x_start, x_end],
-                            y_range=[y_start, y_end])
+                               plot_height=y_end-y_start,
+                               x_range=[x_start, x_end],
+                               y_range=[y_start, y_end])
 
     agg = canvas.points(data, 'x', 'y', agg=reduction_op)
-    img = t_func.shade(agg.m * np.subtract(1.0, agg.s),
-                        cmap=Inferno256,
-                        span=[0,1],
-                        how='linear')
+    img = t_func.shade(np.greater_equal(agg.not_bkg,agg.bkg),
+                       #cmap=Reds3,
+                       span=[0,1],
+                       how='linear')
 
     img_plt = f.image_rgba(image=[img.data],
                         x=x_start,
@@ -200,13 +248,106 @@ legend = Legend(items=items,
 
 f.add_layout(legend, place='right')
 
+probs_grid[1].append(f)
+outputs.append(gridplot(probs_grid))
+
+def get_color(color):
+    cmap = cm.get_cmap(color)
+    return [clr.to_hex(m) for m in cmap(np.arange(cmap.N))]
+# Merge as colors
+reds = get_color('Reds')
+greens = get_color('Greens')
+blues = get_color('Blues')
+
+f = figure(title='R=Spheroid, G=Disk, B=Point Source',
+           x_range=[x_start, x_end],
+           y_range=[y_start, y_end],
+           width=GRAPH_WIDTH,
+           height=GRAPH_HEIGHT,
+           toolbar_location='above')
+
+reduction_op = reductions.summary(sph=reductions.mean('Spheroid'),
+                                  dk=reductions.mean('Disk'),
+                                  ps=reductions.mean('Point Source'),
+                                  bkg=reductions.mean('Background'))
+
+canvas = datashader.Canvas(plot_width=x_end-x_start,
+                           plot_height=y_end-y_start,
+                           x_range=[x_start, x_end],
+                           y_range=[y_start, y_end])
+
+agg = canvas.points(data, 'x', 'y', agg=reduction_op)
+
+imgs = []
+for morph, color in [(agg.sph, 'red'),(agg.dk, 'green'), (agg.ps, 'blue'), (agg.bkg, 'black')]:
+    imgs.append(t_func.shade(morph, cmap=['white', color], span=[0, 1], how='linear'))
+
+img = t_func.stack(*imgs, how='add')
+#img = t_func.set_background(img, color='black')
+
+img_plt = f.image_rgba(image=[img.data],
+                       x=x_start,
+                       y=y_start,
+                       dw=width,
+                       dh=height)
+
+
 outputs.append(f)
 
+
+
 # ==============================================================================
+
+# ==============================================================================
+# plot in-segmap PDFs
+pdf_cdf = []
+f = figure(x_range=[-0.1, 1.1],
+           y_axis_type='log',
+           width=GRAPH_WIDTH,
+           height=GRAPH_HEIGHT,
+           toolbar_location='above',
+           title='PDFs For all Pixels')
+
+for morphology, color in zip(columns[2:], Spectral6):
+    reduction_op = reductions.summary(not_bkg=reductions.mean(morphology),
+                                      bkg=reductions.mean('Background'))
+    canvas = datashader.Canvas(plot_width=x_end-x_start,
+                               plot_height=y_end-y_start,
+                               x_range=[x_start, x_end],
+                               y_range=[y_start, y_end])
+
+    agg = canvas.points(data, 'x', 'y', agg=reduction_op)
+
+    mask = np.greater_equal(agg.not_bkg, agg.bkg)
+    print(mask.where(mask==True, drop=True).x.data)
+
+
+for morphology, color in zip(columns[2:], Spectral6):
+    results, edges = np.histogram(data[morphology].values, bins=1000)
+    x = edges[:-1] + np.diff(edges)/2
+    y = results / results.sum()
+    width = edges[1]-edges[0]
+    f.line(x,y, line_color=color,
+                line_alpha=0.75,
+                line_width=4,
+                legend=morphology)
+
+f.legend.location = 'bottom_right'
+f.legend.click_policy = 'hide'
+pdf_cdf.append(f)
+
+ # plot out-segmap PDFs
+
+
+
+
+# ==============================================================================
+
 
 
 # ==============================================================================
 # Plot PDFs for all pixels
+pdf_cdf = []
 f = figure(x_range=[-0.1, 1.1],
         y_axis_type='log',
         width=GRAPH_WIDTH,
@@ -219,14 +360,39 @@ for morphology, color in zip(columns[2:], Spectral6):
     x = edges[:-1] + np.diff(edges)/2
     y = results / results.sum()
     width = edges[1]-edges[0]
-    f.line(x,y, line_color=color, 
-                line_alpha=0.5, 
-                line_width=4, 
+    f.line(x,y, line_color=color,
+                line_alpha=0.75,
+                line_width=4,
                 legend=morphology)
 
 f.legend.location = 'bottom_right'
 f.legend.click_policy = 'hide'
-outputs.append(f)
+pdf_cdf.append(f)
+
+# Plot CDFs for all pixels
+f = figure(x_range=[-0.1, 1.1],
+           y_axis_type='log',
+           width=GRAPH_WIDTH,
+           height=GRAPH_HEIGHT,
+           toolbar_location='above',
+           title='CDFs For all Pixels')
+
+for morphology, color in zip(columns[2:], Spectral6):
+    results, edges = np.histogram(data[morphology].values, bins=1000)
+    x = edges[:-1] + np.diff(edges)/2
+    y = np.cumsum(results / results.sum())
+    width = edges[1]-edges[0]
+    f.line(x,y, line_color=color,
+                line_alpha=0.75,
+                line_width=4,
+                legend=morphology)
+
+f.legend.location = 'bottom_right'
+f.legend.click_policy = 'hide'
+pdf_cdf.append(f)
+pdf_cdf = row(pdf_cdf)
+
+outputs.append(pdf_cdf)
 # ==============================================================================
 
 output = column(outputs)
